9d3c8d13ef7c311c313cecf36a6f8160f609709a
Brian Arnold <brianarn@gmail.com>
2013-07-18 20:37:53 -0600

    First sorta working version

--- app/main.js
+++ app/main.js
@@ -1,4 +1,35 @@
-define([
-], function(){
-	console.log('Wooooo');
+// Ember mods
+Ember.Handlebars.helper('capfirst', function(value, options){
+  return value.substr(0,1).toUpperCase() + value.substr(1);
+});
+
+// Application
+App = Ember.Application.create();
+
+App.Router.map(function(){
+  this.resource('stats');
+});
+
+App.StatsRoute = Ember.Route.extend({
+  model: function(){
+    return [{
+      name: 'tires',
+      value: 6
+    },{
+      name: 'brakes',
+      value: 3
+    },{
+      name: 'gearbox',
+      value: 3
+    },{
+      name: 'body',
+      value: 3
+    },{
+      name: 'engine',
+      value: 3
+    },{
+      name: 'suspension',
+      value: 2
+    }];
+  }
 });
